7565d533c4ac06096291ac707f580d7ddb9c37ef
Beau Charman <beau.charman@gmail.com>
2013-03-12 18:53:31 +1100

    Updates to init function

--- library/core/initial-theme-setup.php
+++ library/core/initial-theme-setup.php
@@ -10,32 +10,36 @@
 add_action('after_setup_theme', 'lt3_initial_theme_setup');
 function lt3_initial_theme_setup()
 {
+  /* Only need to run this once */
   if(get_option('theme_setup_status') !== '1')
   {
+    /* Set the WordPress options the way you like */
     $core_settings = array(
-      'avatar_default'    => 'mystery',
-      'avatar_rating'     => 'G',
-      'default_role'      => 'author',
-      'comments_per_page' => '20',
-      'use_smilies'       => '0',
-      'timezone_string'   => 'Australia/Sydney',
-      'posts_per_page'    => '20',
-      'large_size_h'      => LT3_PAGE_CONTENT_WIDTH,
-      'large_size_w'      => LT3_PAGE_CONTENT_WIDTH * 1.25,
-      'medium_size_h'     => LT3_PAGE_CONTENT_WIDTH / 2,
-      'medium_size_w'     => LT3_PAGE_CONTENT_WIDTH,
-      'thumbnail_crop'    => '1',
-      'thumbnail_size_h'  => LT3_PAGE_CONTENT_WIDTH / 4,
-      'thumbnail_size_w'  => LT3_PAGE_CONTENT_WIDTH / 4,
-      'time_format'       => 'g:i a',
-      'date_format'       => 'd/m/Y',
-      'gzipcompression'   => '1',
-      'blog_public'       => '0',
-      'permalink_structure' => '/%postname%/'
+      'avatar_default'      => 'mystery',
+      'avatar_rating'       => 'G',
+      'blog_public'         => '0',
+      'comments_per_page'   => '20',
+      'date_format'         => 'd/m/Y',
+      'default_role'        => 'author',
+      'gzipcompression'     => '1',
+      'large_size_h'        => LT3_PAGE_CONTENT_WIDTH,
+      'large_size_w'        => LT3_PAGE_CONTENT_WIDTH * 1.25,
+      'medium_size_h'       => LT3_PAGE_CONTENT_WIDTH / 2,
+      'medium_size_w'       => LT3_PAGE_CONTENT_WIDTH,
+      'permalink_structure' => '/%postname%/',
+      'posts_per_page'      => '20',
+      'thumbnail_crop'      => '1',
+      'thumbnail_size_h'    => LT3_PAGE_CONTENT_WIDTH / 4,
+      'thumbnail_size_w'    => LT3_PAGE_CONTENT_WIDTH / 4,
+      'time_format'         => 'g:i a',
+      'timezone_string'     => 'Australia/Sydney',
+      'use_smilies'         => '0'
     );
     foreach ($core_settings as $key => $value) {
       update_option( $key, $value );
     }
+
+    /* Delete the example post, page and comment */
     wp_delete_post(1, true);
     wp_delete_post(2, true);
     wp_delete_comment(1);
@@ -44,7 +48,23 @@ function lt3_initial_theme_setup()
       require_once(ABSPATH.'wp-admin/includes/file.php');
       delete_plugins(array('hello.php'));
     }
+    /* Update the status so this dosn't run again */
     update_option( 'theme_setup_status', '1' );
 
+    /* Lets let the admin know whats going on. */
+    $msg = '
+    <div class="error">
+      <p>The ' . get_option( 'current_theme' ) . 'theme has changed your WordPress default <a href="' . admin_url( 'options-general.php' ) . '" title="See Settings">settings</a> and deleted default posts & comments.</p>
+    </div>';
+    add_action( 'admin_notices', $c = create_function( '', 'echo "' . addcslashes( $msg, '"' ) . '";' ) );
+  }
+
+  /* Else if we are re-activing the theme */
+  elseif ( $the_theme_status === '1' and isset( $_GET['activated'] ) ) {
+    $msg = '
+    <div class="updated">
+      <p>The ' . get_option( 'current_theme' ) . ' theme was successfully re-activated.</p>
+    </div>';
+    add_action( 'admin_notices', $c = create_function( '', 'echo "' . addcslashes( $msg, '"' ) . '";' ) );
   }
 }
