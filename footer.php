d785e4941f695853b7ceef6678735feeea7a7cff
Beau <beau.charman@gmail.com>
2013-04-02 17:09:18 +1200

    Update footer.php

--- footer.php
+++ footer.php
@@ -17,6 +17,8 @@
   <footer class="page-footer">
 
     <?php if(is_active_sidebar('footer-sidebar-widgets')) dynamic_sidebar('footer-sidebar-widgets'); ?>
+    
+    <?php lt3_page_footer_menu(); ?>
 
     <p class="site-information">&copy;<?php echo date('Y'); ?>&nbsp;<?php echo bloginfo('name'); ?></p>
 
