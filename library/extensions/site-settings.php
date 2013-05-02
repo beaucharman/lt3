8c2bcfb50b01a1451a7bf24ae0411232bd1bed22
Beau Charman <beau.charman@gmail.com>
2013-05-02 11:40:09 +1000

    remove unneeded tags

--- library/extensions/site-settings.php
+++ library/extensions/site-settings.php
@@ -212,8 +212,7 @@ class LT3_Site_Settings_Page
 
           /* default */
           default:
-            echo '<tr><td colspan="2"><span style="color: red;">Sorry, '
-              . 'the type allocated for this input is not valid.</span></td></tr>';
+            echo '<span style="color: red;">Sorry, the type allocated for this input is not valid.</span>';
             break;
 
         } // end switch
