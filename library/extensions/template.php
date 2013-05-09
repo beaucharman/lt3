13d35ece40ebd0f45a0762d753d0a38bc5fd0feb
Beau <beau.charman@gmail.com>
2013-05-09 12:31:24 +1000

    Update template.php

--- library/extensions/template.php
+++ library/extensions/template.php
@@ -656,30 +656,32 @@ function lt3_include_post_meta()
  * ------------------------------------------------------------------------ */
 function lt3_excerpt( $raw_text = '', $echo_result = true, $output_limit = LT3_EXCERPT_LENGTH )
 {
-	$text = explode( ' ', $raw_text );
-	$ellipses = false;
-	if ( sizeof( $text ) > $output_limit )
+  $text = explode( ' ', $raw_text );
+  $ellipses = false;
+  if ( sizeof( $text ) > $output_limit )
   {
     for( $counter = sizeof( $text ); $counter > $output_limit; $counter-- )
     {
       array_pop( $text );
-		}
-		$output_text = implode( ' ', $text );
-		$ellipses = true;
-	}
-	else
-	{
-		$output_text = implode( ' ', $text );
-	}
-	if ( $ellipses )
-	{
-		$output_text .= '&hellip;';
-	}
-	if ( ! $echo_result )
-	{
-		return $output_text;
-	}
-	echo	$output_text;
+    }
+    $output_text = implode( ' ', $text );
+    $ellipses = true;
+  }
+  else
+  {
+    $output_text = implode( ' ', $text );
+  }
+  
+  if ( $ellipses ) 
+  { 
+    $output_text .= '&hellip;';
+  }
+
+  if ( ! $echo_result )
+  {
+    return $output_text;
+  }
+  echo $output_text;
 }
 
 /* Remove empty paragraph tags from the_content
