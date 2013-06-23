bf701dbd00efe3757e0a0ec3bbfcb7fb034090f6
Beau Charman <beau.charman@gmail.com>
2013-06-23 14:32:56 +1000

    Changed comments setting

--- library/project/config.php
+++ library/project/config.php
@@ -48,7 +48,7 @@ define('LT3_EXCERPT_MORE', 'more &rarr;');
 
 /* Enable global comments
    ======================================================================== */
-define('LT3_ENABLE_GLOBAL_COMMENTS', true);
+define('LT3_ENABLE_GLOBAL_COMMENTS', false);
 
 /* Enable site search
    ======================================================================== */
