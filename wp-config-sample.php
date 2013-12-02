45f01819ef498f5a01e48215e83c639e2e1fc23b
Beau <beau.charman@gmail.com>
2013-12-03 09:49:44 +1100

    Update wp-config-sample.php

--- wp-config-sample.php
+++ wp-config-sample.php
@@ -236,7 +236,7 @@ define('WPLANG', '');
 
 /** Absolute path to the WordPress directory. */
 if ( !defined('ABSPATH') )
-	define('ABSPATH', dirname(__FILE__) . '/');
+  define('ABSPATH', dirname(__FILE__) . '/');
 
 /** Sets up WordPress vars and included files. */
 require_once(ABSPATH . 'wp-settings.php');
