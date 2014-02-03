12e63cc837255e3cddad2290891d8df842cd6039
Beau Charman <beau.charman@gmail.com>
2014-02-03 19:21:25 +1100

    Update loop-taxonomy.php

--- lt3/library/views/loop-taxonomy.php
+++ lt3/library/views/loop-taxonomy.php
@@ -14,28 +14,24 @@
 
 <?php while (have_posts()) : the_post(); ?>
 
-<article <?php post_class('article article__taxonomy taxonomy entry excerpt post-' . get_the_ID()); ?>>
+<article <?php post_class('article archive-article article__taxonomy taxonomy entry excerpt post-' . get_the_ID()); ?>>
 
-  <h2 class="article-title">
-    <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>">
-      <?php the_title(); ?>
-    </a>
+  <h2 class="article-heading archive-article__heading">
+    <a href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
   </h2>
 
   <?php Samurai_Snippet::include_post_meta(); ?>
 
   <?php if (has_post_thumbnail()) : ?>
-  <figure class="featured-image">
+  <figure class="featured-image archive-article__featured-image">
     <?php the_post_thumbnail('thumbnail'); ?>
   </figure>
   <?php endif; ?>
 
   <?php the_excerpt(); ?>
 
-  <footer class="article-footer">
-
+  <footer class="article-footer archive-article__footer">
     <a class="read-more" href="<?php the_permalink() ?>" title="<?php the_title_attribute(); ?>"><?php Samurai_Snippet::read_more_text(); ?></a>
-
   </footer>
 
 </article>
